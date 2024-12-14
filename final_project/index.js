368d072ffcc80e89342d0fbe61d1d41ba3cdb9c9
M-Marcer <marco.marcer@gmail.com>
2024-12-14 00:47:54 -0500

    Step 7 completed

--- final_project/index.js
+++ final_project/index.js
@@ -19,7 +19,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
                 req.user = user;
-                res.send("Current token :" + token);
+                //res.send("Current token :" + token);
                 next(); // Proceed to the next middleware
             } else {
                 return res.status(403).json({ message: "User not authenticated" });
